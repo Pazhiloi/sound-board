8556d4619552e0750fdbde602765eba24b93a3b4
Pazhiloi <martinsen06@gmail.com>
2022-02-10 15:37:49 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-"use strict";
+const sounds = ['one', 'two','three','four','five','six']
+
+sounds.forEach(sound => {
+  const btn = document.createElement('button')
+  btn.classList.add('btn')
+
+  btn.innerText = sound
+
+  btn.addEventListener('click', () => {
+    stopSongs()
+
+    document.getElementById(sound).play()
+  })
+
+  document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs(params) {
+  sounds.forEach(sound => {
+    const song = document.getElementById(sound)
+
+    song.pause()
+    song.currentTime = 0
+  })
+}
